7bdfc88da9af0f0fb86a2fecc1fd858fe111c297
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-05 16:24:48 +0100

    serve json

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"})
+})
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
