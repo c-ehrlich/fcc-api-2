3d016d8b81861780d0e6c14d781fca7c8ae5a2dc
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-05 17:31:37 +0100

    create logging middleware

--- myApp.js
+++ myApp.js
@@ -1,11 +1,22 @@
-// require("dotenv").config();
-
 var express = require("express");
 var app = express();
 
 console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
+
+// middleware to use dotenv in local testing
+app.get("*", (req, res, next) => {
+  if (req.ip === "::1") {
+    require("dotenv").config();
+  }
+  next();
+})
+
+app.get("*", (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+});
 
 app.get("/json", (req, res) => {
   res.json({
